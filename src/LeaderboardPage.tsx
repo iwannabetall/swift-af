7e6f55f0a8fcbf9ce77cbfde2fb1401a766bc1d0
iwannabetall <iwannabetall@gmail.com>
2023-12-20 14:08:44 -0500

    center leaderboard pls

--- src/LeaderboardPage.tsx
+++ src/LeaderboardPage.tsx
@@ -54,7 +54,7 @@ function LeaderboardPage() {
 					<img src={title} className='mx-auto logo p-4 max-h-32' alt="Swift AF" />	
 				</div>
 				<h2>Wait, don't go. Getting the swiftest swifties!</h2></div>}
-			{!isLoading && <div className='grid grid-cols-1 p-2 items-center mt-20 lg:ml-8 lg:mr-8 sm:ml-2 sm:mr-2'>
+			{!isLoading && <div className='grid grid-cols-1 p-2 justify-items-center mt-20 lg:ml-8 lg:mr-8 sm:ml-2 sm:mr-2 justify-center'>
 				
 				<div className=''>
 					<img src={title} className='mx-auto logo p-4 max-h-32' alt="Swift AF" />				
